161920b1fcf8aee742f7ce37f86956307f0cd049
lesktimo <l3skt1m@gmail.com>
2017-01-26 19:59:50 +0200

    typo

--- src/main/java/sec/project/controller/AdminController.java
+++ src/main/java/sec/project/controller/AdminController.java
@@ -1,13 +1,9 @@
 package sec.project.controller;
 
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sec.project.domain.Account;
 import sec.project.repository.AccountRepo;
 
 @Controller
@@ -15,7 +11,6 @@ public class AdminController {
 
     private AccountRepo repo;
 
-    @Profile("heroku")
     @RequestMapping("/admin")
     public String adminMappingHeroku(Model model) throws URISyntaxException {
         model.addAttribute("accounts", repo.findAll());
