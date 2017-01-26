2475c584b6d9884b3ed3bd86152fa476bdfbcf94
lesktimo <l3skt1m@gmail.com>
2017-01-26 17:38:13 +0200

    hotfix2

--- src/main/java/sec/project/service/AccountService.java
+++ src/main/java/sec/project/service/AccountService.java
@@ -1,37 +1,35 @@
 package sec.project.service;
 
-import java.net.URISyntaxException;
-import java.sql.SQLException;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sec.project.db.Database;
 import sec.project.domain.Account;
+import sec.project.repository.AccountRepo;
 
 @Service
 public class AccountService {
 
     @Autowired
-    private Database db;
+    private AccountRepo accRepo;
 
     @PostConstruct
-    public void init() throws SQLException, URISyntaxException {
-        if (db.loadUser("testi") == null) {
+    public void init() {
+        if (accRepo.findByUsername("testi") == null) {
             Account acc = new Account();
             acc.setUsername("testi");
             acc.setPassword("testi");
-            db.saveUser(acc.getUsername(), acc.getPassword());
+            accRepo.save(acc);
         }
     }
 
-    public void save(Account acc) throws SQLException, URISyntaxException {
+    public void save(Account acc) {
         acc.setUsername(acc.getUsername());
         acc.setPassword(acc.getPassword());
-        db.saveUser(acc.getUsername(), acc.getPassword());
+        accRepo.save(acc);
     }
 
-    public Account findByUsername(String username) throws SQLException, URISyntaxException {
-        return db.loadUser(username);
+    public Account findByUsername(String username) {
+        return accRepo.findByUsername(username);
     }
 
 }
--- src/main/java/sec/project/service/CustomUserDetailsService.java
+++ src/main/java/sec/project/service/CustomUserDetailsService.java
@@ -1,10 +1,6 @@
 package sec.project.service;
 
-import java.net.URISyntaxException;
-import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,35 +8,30 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import sec.project.db.Database;
 import sec.project.domain.Account;
+import sec.project.repository.AccountRepo;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
-    private Database db;
+    private AccountRepo accRepo;
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        try {
-            Account account = db.loadUser(username);
-            if (account == null) {
-                throw new UsernameNotFoundException("No such user: " + username);
-            }
-            return new org.springframework.security.core.userdetails.User(
-                    account.getUsername(),
-                    account.getPassword(),
-                    true,
-                    true,
-                    true,
-                    true,
-                    Arrays.asList(new SimpleGrantedAuthority("USER")));
-        } catch (SQLException | URISyntaxException ex) {
-            Logger.getLogger(CustomUserDetailsService.class.getName()).log(Level.SEVERE, null, ex);
+        Account account = accRepo.findByUsername(username);
+        if (account == null) {
+            throw new UsernameNotFoundException("No such user: " + username);
         }
-        throw new UsernameNotFoundException("No such user: " + username);
+        return new org.springframework.security.core.userdetails.User(
+                account.getUsername(),
+                account.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                Arrays.asList(new SimpleGrantedAuthority("USER")));
     }
 
 }
